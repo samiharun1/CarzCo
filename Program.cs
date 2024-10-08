38370f36ecf1457ee55b210e30948d29dcbb0480
samih <samih@LAPTOP-UHMUODJD>
2024-10-08 13:21:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Transactions;
+
+namespace CarzCo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicleList = new List<Vehicle>();
+
+            vehicleList.Add(new Car("volvo", "V60", 2000, 5));
+            vehicleList.Add(new Motorcycle("Harley Davis", "s150", 2005, true));
+            vehicleList.Add(new Truck("Nissan", "T6000", 2009, 3));
+
+            var newList = FilterVehicles<Motorcycle>(vehicleList);
+            PrintAll(vehicleList);
+           // PrintAll(newList);
+            var vehicleToSell = vehicleList.Find(v => v.Brand == "Nissan");
+            SellVehicle(vehicleList, vehicleToSell);
+            PrintAll(vehicleList);
+
+        }
+        static List<T> FilterVehicles<T>(List<Vehicle> vehicleList) where T : Vehicle
+
+        {
+            return vehicleList.OfType<T>().ToList();
+           
+        }
+        static void SellVehicle(List<Vehicle> vehicleList, Vehicle vehicle)
+        {
+            Console.WriteLine($"Säljer fordonet av märke {vehicle.Brand}");
+            vehicleList.Remove(vehicle);
+        }
+        static void PrintAll<T>(List<T> vehicleList) where T : Vehicle
+        {
+            foreach (var vehicle in vehicleList)
+            {
+                vehicle.PrintInfo();
+            }
+        }
+    }
+}
+
---
+++ Vehicle.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CarzCo
+{
+    public abstract class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Vehicle(string brand, string model, int year)
+        {
+            //Konstruktor
+            Brand = brand;
+            Model = model;
+            Year = year;
+
+        }
+        public virtual void PrintInfo()
+        {
+            Console.Write($"{this.Brand} {this.Model} {this.Year} ");
+        }
+
+    }
+    public interface IDriveable
+    {
+        void Drive();
+        
+    }
+    public class Car : Vehicle, IDriveable
+    {
+        public int NumberOfDoors;
+        public Car(string brand, string model, int year, int numberOfDoors) : base(brand, model, year)
+        {
+            NumberOfDoors = numberOfDoors;
+
+
+        }
+
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"bilen har {this.NumberOfDoors} dörrar.");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Bilen kör.");
+        }
+    }
+    public class Motorcycle : Vehicle, IDriveable
+    {
+        public bool HasSideCar;
+        public Motorcycle(string brand, string model, int year, bool hasSideCar) : base(brand, model, year)
+        {
+            HasSideCar = hasSideCar;
+
+
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Motorcycle kör.");
+        }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            if (HasSideCar)
+            {
+                Console.WriteLine("motorcyckeln har en sidvagn.");
+            }
+            else 
+            {
+                Console.WriteLine("motorcyckeln har inte en sidvagn.");
+            }
+        }
+    }
+    public class Truck : Vehicle, IDriveable
+    {
+        public int WheelAxis;
+        public Truck(string brand, string model, int year, int wheelAxis) : base(brand, model, year)
+        {
+            WheelAxis = wheelAxis;
+
+
+        }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"lastbilen har {this.WheelAxis} hjulaxlar.");
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Truck kör.");
+        }
+    }
+
+}
+
